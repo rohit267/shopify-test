3f6021d1e567114c1298fd86640adaedcc5fe182
Rohit Mahto <rohitmahto267@gmaill.com>
2021-06-14 23:32:20 +0530

    first

---
+++ src/components/Homepage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CSS from './index.module.css'
+
+function Homepage(props) {
+    return (
+        <div className={CSS.Homepage}>
+
+        </div>
+    );
+}
+
+export default Homepage;
